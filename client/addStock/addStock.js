0e67b0f472c3107410d3975e015ebda5bdfb2e9a
Bill Monsueir <bmonsueir@gmail.com>
2016-01-05 12:46:59 -0800

    added responsiveness

--- client/addStock/addStock.js
+++ client/addStock/addStock.js
@@ -4,19 +4,13 @@
       event.preventDefault();
       var stockEntry = $('[name=stockEntry]').val().toUpperCase();
 
-      //var stockEntry = event.target.stockEntry.value;
-    Meteor.call("findName", stockEntry, function(error, result){
-      if(result !== null){
-       Stocks.insert({
-            name: result,
-            symbol: stockEntry
+        Meteor.call("findName", stockEntry, function(error, result){
+          if(result !== null){
+              Stocks.insert({
+                name: result,
+                symbol: stockEntry
+                });
+              }
           });
         }
-        location.reload();
-      //  $('[name=stockEntry]').val().clear();
-    });
-
-  
-
-  }
   });
--- client/companies/companies.js
+++ client/companies/companies.js
@@ -1,34 +1,31 @@
-var companySize;
-
 Template.companies.helpers({
   name: function () {
-    var companies = [];
-    companies = Stocks.find().fetch();
-    companySize = companies.length;
-    return companies;
-  }
-
+    var svg = d3.select("#graph");
+    svg.select("*").remove();
+    makeGraph();
+    return Stocks.find().fetch();
+    }
 });
 
 Template.companies.events({
   'click .company0': function(event){
+    event.preventDefault();
     Stocks.remove(this._id);
-    location.reload();
   },
   'click .company1': function(event){
+    event.preventDefault();
     Stocks.remove(this._id);
-    location.reload();
   },
   'click .company2': function(event){
+    event.preventDefault();
     Stocks.remove(this._id);
-    location.reload();
   },
   'click .company3': function(event){
+    event.preventDefault();
     Stocks.remove(this._id);
-    location.reload();
   },
   'click .company4': function(event){
+    event.preventDefault();
     Stocks.remove(this._id);
-    location.reload();
   }
 });
